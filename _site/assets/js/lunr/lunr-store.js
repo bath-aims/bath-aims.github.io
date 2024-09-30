8a2f46ceebbe7e958ae4c35ffa45cad4aa0e31b0
jshelton4 <j.shelton@bath.ac.uk>
2024-09-30 11:36:26 +0100

    Hopefully fixed "Friday 12:15" -> "Tuesday 13:15"

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,277 @@
+var store = [{
+        "title": "Adams_b",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/members/adams_b",
+        "teaser": null
+      },{
+        "title": "Bowness_r",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/members/bowness_r",
+        "teaser": null
+      },{
+        "title": "Budd_c",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/members/budd_c",
+        "teaser": null
+      },{
+        "title": "Cherednichenko_k",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/members/cherednichenko_k",
+        "teaser": null
+      },{
+        "title": "Dawes_j",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/members/dawes_j",
+        "teaser": null
+      },{
+        "title": "Doak_a",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/members/doak_a",
+        "teaser": null
+      },{
+        "title": "Evans_j",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/members/evans_j",
+        "teaser": null
+      },{
+        "title": "Hester_e",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/members/hester_e",
+        "teaser": null
+      },{
+        "title": "Kreusser_l",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/members/kreusser_l",
+        "teaser": null
+      },{
+        "title": "Matthies_k",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/members/matthies_k",
+        "teaser": null
+      },{
+        "title": "Rogers_t",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/members/rogers_t",
+        "teaser": null
+      },{
+        "title": "Shelton_j",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/members/shelton_j",
+        "teaser": null
+      },{
+        "title": "Sivaloganathan_j",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/members/sivaloganathan_j",
+        "teaser": null
+      },{
+        "title": "Tang_h",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/members/tang_h",
+        "teaser": null
+      },{
+        "title": "Trinh_p",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/members/trinh_p",
+        "teaser": null
+      },{
+        "title": "Tweedy_j",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/members/tweedy_j",
+        "teaser": null
+      },{
+        "title": "Wheeler_m",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/members/wheeler_m",
+        "teaser": null
+      },{
+        "title": "Wilson_s",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/members/wilson_s",
+        "teaser": null
+      },{
+        "title": "Yates_k",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/members/yates_k",
+        "teaser": null
+      },{
+        "title": "Bath Applied and Interdisciplinary Mathematics Seminar",
+        "excerpt":"Fridays at 12.15 at Wolfson 4W 1.7. All talks will be broadcast on Zoom. Everyone is welcome at these talks. Date Speaker Title 8 Oct 2024 Nabil Fadai (Nottingham) Stochastic agent-based models in mathematical biology In the last decade, there has been a movement to describe biological and social systems...","categories": ["bnaseminar"],
+        "tags": [],
+        "url": "/categories/2024/seminar_10_01",
+        "teaser": null
+      },{
+        "title": "Analysis of PDEs",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/research/analysis",
+        "teaser": null
+      },{
+        "title": "Asymptotic Analysis",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/research/asymptotics",
+        "teaser": null
+      },{
+        "title": "Calculus of Variations",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/research/calcvar",
+        "teaser": null
+      },{
+        "title": "Dynamical Systems",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/research/ds",
+        "teaser": null
+      },{
+        "title": "Fluid mechanics",
+        "excerpt":"           ","categories": [],
+        "tags": [],
+        "url": "/research/fluids",
+        "teaser": null
+      },{
+        "title": "Mathematical biology",
+        "excerpt":"           ","categories": [],
+        "tags": [],
+        "url": "/research/mathbio",
+        "teaser": null
+      },{
+        "title": "Mathematical Modelling",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/research/modelling",
+        "teaser": null
+      },{
+        "title": "Numerical Analysis",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/research/na",
+        "teaser": null
+      },{
+        "title": "Applied mathematics 1A",
+        "excerpt":"#This course focuses on time dependent (hyperbolic and parabolic) PDEs, introducing time-stepping methods and approximations in space to find effective computational methods.  ","categories": [],
+        "tags": [],
+        "url": "/teaching/MA12002",
+        "teaser": null
+      },{
+        "title": "Applied mathematics 1B",
+        "excerpt":"#This course focuses on time dependent (hyperbolic and parabolic) PDEs, introducing time-stepping methods and approximations in space to find effective computational methods.  ","categories": [],
+        "tags": [],
+        "url": "/teaching/MA12003",
+        "teaser": null
+      },{
+        "title": "Differential equations and vector calculus",
+        "excerpt":"#This course focuses on time dependent (hyperbolic and parabolic) PDEs, introducing time-stepping methods and approximations in space to find effective computational methods.  ","categories": [],
+        "tags": [],
+        "url": "/teaching/MA22016",
+        "teaser": null
+      },{
+        "title": "Machine learning 1",
+        "excerpt":"#This course focuses on time dependent (hyperbolic and parabolic) PDEs, introducing time-stepping methods and approximations in space to find effective computational methods.  ","categories": [],
+        "tags": [],
+        "url": "/teaching/MA22018",
+        "teaser": null
+      },{
+        "title": "Advanced linear algebra",
+        "excerpt":"#This course focuses on time dependent (hyperbolic and parabolic) PDEs, introducing time-stepping methods and approximations in space to find effective computational methods.  ","categories": [],
+        "tags": [],
+        "url": "/teaching/MA22020",
+        "teaser": null
+      },{
+        "title": "Partial differential equations",
+        "excerpt":"#This course focuses on time dependent (hyperbolic and parabolic) PDEs, introducing time-stepping methods and approximations in space to find effective computational methods.  ","categories": [],
+        "tags": [],
+        "url": "/teaching/MA22021",
+        "teaser": null
+      },{
+        "title": "Mathematical biology ecology and epidemiology",
+        "excerpt":"#This course focuses on time dependent (hyperbolic and parabolic) PDEs, introducing time-stepping methods and approximations in space to find effective computational methods.  ","categories": [],
+        "tags": [],
+        "url": "/teaching/MA22035",
+        "teaser": null
+      },{
+        "title": "Mathematical methods 1",
+        "excerpt":"#This course focuses on time dependent (hyperbolic and parabolic) PDEs, introducing time-stepping methods and approximations in space to find effective computational methods.  ","categories": [],
+        "tags": [],
+        "url": "/teaching/MA30044",
+        "teaser": null
+      },{
+        "title": "Mathematical biology 1",
+        "excerpt":"#This course focuses on time dependent (hyperbolic and parabolic) PDEs, introducing time-stepping methods and approximations in space to find effective computational methods.  ","categories": [],
+        "tags": [],
+        "url": "/teaching/MA30047",
+        "teaser": null
+      },{
+        "title": "Mathematical methods 2",
+        "excerpt":"#This course focuses on time dependent (hyperbolic and parabolic) PDEs, introducing time-stepping methods and approximations in space to find effective computational methods.  ","categories": [],
+        "tags": [],
+        "url": "/teaching/MA30059",
+        "teaser": null
+      },{
+        "title": "Dynamics and chaos",
+        "excerpt":"#This course focuses on time dependent (hyperbolic and parabolic) PDEs, introducing time-stepping methods and approximations in space to find effective computational methods.  ","categories": [],
+        "tags": [],
+        "url": "/teaching/MA30060",
+        "teaser": null
+      },{
+        "title": "Mathematical biology 2",
+        "excerpt":"#This course focuses on time dependent (hyperbolic and parabolic) PDEs, introducing time-stepping methods and approximations in space to find effective computational methods.  ","categories": [],
+        "tags": [],
+        "url": "/teaching/MA30063",
+        "teaser": null
+      },{
+        "title": "Numerical solution of elliptic PDEs",
+        "excerpt":"#This course focuses on time dependent (hyperbolic and parabolic) PDEs, introducing time-stepping methods and approximations in space to find effective computational methods.  ","categories": [],
+        "tags": [],
+        "url": "/teaching/MA30170",
+        "teaser": null
+      },{
+        "title": "Continuum mechanics",
+        "excerpt":"This course focuses on time dependent (hyperbolic and parabolic) PDEs, introducing time-stepping methods and approximations in space to find effective computational methods.  ","categories": [],
+        "tags": [],
+        "url": "/teaching/MA30253",
+        "teaser": null
+      },{
+        "title": "Mathematics of Planet Earth",
+        "excerpt":"#This course focuses on time dependent (hyperbolic and parabolic) PDEs, introducing time-stepping methods and approximations in space to find effective computational methods.  ","categories": [],
+        "tags": [],
+        "url": "/teaching/MA30287",
+        "teaser": null
+      },{
+        "title": "Elasticity",
+        "excerpt":"#This course focuses on time dependent (hyperbolic and parabolic) PDEs, introducing time-stepping methods and approximations in space to find effective computational methods.  ","categories": [],
+        "tags": [],
+        "url": "/teaching/MA40049",
+        "teaser": null
+      },{
+        "title": "Numerical solution of evolutionary equations",
+        "excerpt":"This course focuses on time dependent (hyperbolic and parabolic) PDEs, introducing time-stepping methods and approximations in space to find effective computational methods.  ","categories": [],
+        "tags": [],
+        "url": "/teaching/MA40171",
+        "teaser": null
+      },{
+        "title": "Viscous fluid dynamics",
+        "excerpt":"#This course focuses on time dependent (hyperbolic and parabolic) PDEs, introducing time-stepping methods and approximations in space to find effective computational methods.  ","categories": [],
+        "tags": [],
+        "url": "/teaching/MA40255",
+        "teaser": null
+      }]
